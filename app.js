dd9360be1d0aab39b0c6c627d3305be7259c4412
Michael Hebblethwaite <mhhebble@gmail.com>
2018-03-27 18:15:14 +1100

    create basic game loop, no win condition yet

--- app.js
+++ app.js
@@ -11,7 +11,45 @@ if that section is free, then player token appears
 check win conditon
 if there are three player tokens in a row, then player wins
 if not, toggle player and return to start of loop
+
+gameboard
+
+0|1|2
+-----
+3|4|5
+-----
+6|7|8
+
+
+
+
 */
+var scoreSheet = new Array(9).fill('');
+var whichPlayer = 'X';
+
+var playMove = function(currentPlayer, nextPlayer, squareId) {
+  scoreSheet[squareId] = currentPlayer;
+  whichPlayer = nextPlayer;
+}
+
+var chooseSpace = function(event){
+  var squareId = Number(event.target.getAttribute('data-id'));
+  if (whichPlayer === 'X') {
+    playMove('X', 'O', squareId);
+    //set image
+    event.target.removeEventListener('click', chooseSpace);
+    //check win conditions
+  } else if (whichPlayer === 'O') {
+    playMove('O','X', squareId);
+    //setimage
+    event.target.removeEventListener('click', chooseSpace);
+    //check win condition
+  }
+  console.log(scoreSheet);
+}
+
+
+
 
 
 var assignToken = function(event) {
@@ -34,8 +72,10 @@ var gameBoard = document.querySelector('.game-board');
 
 for (var i = 0; i < 9; i++) {
   var gameSquare = document.createElement('div');
-  gameSquare.addEventListener('click', assignToken);
+  gameSquare.addEventListener('click', chooseSpace);
   gameSquare.className = 'square';
   gameSquare.setAttribute('data-id',i);
   gameBoard.appendChild(gameSquare);
 }
+
+
