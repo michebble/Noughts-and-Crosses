0eaff7d8f1eddfb400ce67fa28fe642c8204131b
Michael Hebblethwaite <mhhebble@gmail.com>
2018-03-29 11:09:25 +1100

    abstract game loop

--- app.js
+++ app.js
@@ -40,9 +40,9 @@ places to check for win
 
 var playArea = new Array(9).fill('emptySquare');
 var winningCombinations = [[0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,4,6],[2,5,8],[3,4,5],[6,7,8]];
-var whichPlayer = 'cross';
+var currentPlayer = 'cross';
 
-var playMove = function(currentPlayer,  squareId) {
+var storeMove = function(currentPlayer,  squareId) {
   playArea[squareId] = currentPlayer;
 }
 
@@ -85,12 +85,12 @@ var assignPointsForRound = function(winner) {
   setTimeout(clearBoard, 1000);
 }
 
-var placeMark = function(markName) {
+var placePlayerSymbol = function(markName) {
   event.target.style.backgroundImage = "url('" + markName + ".png')";
   event.target.style.backgroundSize = "contain"; 
   event.target.classList.remove('rotateIn');
   event.target.classList.add('jello');
-  event.target.removeEventListener('click', chooseSpace);
+  event.target.removeEventListener('click', activeSquare);
 }
 
 
@@ -98,37 +98,35 @@ var placeMark = function(markName) {
 var disableBoard = function() {
   var squareToDisable = document.querySelectorAll('.square');
   for (var i =0; i < 9; i++) {
-    squareToDisable[i].removeEventListener('click', chooseSpace);
+    squareToDisable[i].removeEventListener('click', activeSquare);
   }
 }
 
-var chooseSpace = function(event){
+var claimSquare = function (currentPlayer, squareId) {
+  storeMove(currentPlayer, squareId);
+  placePlayerSymbol(currentPlayer);
+}
+
+var isGameOver = function(currentPlayer) {
+  if (winCondition() === true) {
+    disableBoard();
+    assignPointsForRound(currentPlayer);
+  } else if (winCondition() === false && playArea.indexOf('emptySquare') === -1) {
+    assignPointsForRound('none');
+  } 
+}
+
+var activeSquare = function(event){
   var squareId = Number(event.target.getAttribute('data-id'));
-  if (whichPlayer === 'cross') {
-    playMove(whichPlayer, squareId);
-    placeMark(whichPlayer);
-    if (winCondition() === true) {
-      disableBoard();
-      assignPointsForRound(whichPlayer);
-    } else if (winCondition() === false && playArea.indexOf('emptySquare') === -1) {
-      console.log("game over");
-      assignPointsForRound('none');
-      setTimeout(clearBoard, 1000);
-    } 
-    whichPlayer = 'naught';
-  } else if (whichPlayer === 'naught') {
-    playMove(whichPlayer, squareId);
-    placeMark(whichPlayer);
-    if (winCondition() === true) { 
-      assignPointsForRound(whichPlayer);
-    } else if (winCondition() === false && playArea.indexOf('emptySquare') === -1) {
-      console.log("game over");
-      assignPointsForRound('none');
-      setTimeout(clearBoard, 1000);
-    } 
-    whichPlayer = 'cross';
+  if (currentPlayer === 'cross') {
+    claimSquare(currentPlayer, squareId);
+    isGameOver(currentPlayer);
+    currentPlayer = 'naught';
+  } else if (currentPlayer === 'naught') {
+    claimSquare(currentPlayer, squareId);
+    isGameOver(currentPlayer);
+    currentPlayer = 'cross';
   }
-  console.log(playArea);
 }
 //  (playArea.indexOf('emptySquare') === -1)
 
@@ -156,7 +154,7 @@ var naughtsScoreSheet = document.querySelector('.player-twos-area');
 var createBoard = function() {
   for (var i = 0; i < 9; i++) {
     var gameSquare = document.createElement('div');
-    gameSquare.addEventListener('click', chooseSpace);
+    gameSquare.addEventListener('click', activeSquare);
     gameSquare.className = 'square';
     gameSquare.setAttribute('data-id',i);
     gameSquare.classList.add('animated');
@@ -171,7 +169,7 @@ var clearBoard =function() {
     gameBoard.removeChild(squaresToClear[i]);
   }
   playArea = new Array(9).fill('emptySquare');
-  // whichPlayer = 'cross';
+  // currentPlayer = 'cross';
   createBoard();
 }
 createBoard();
